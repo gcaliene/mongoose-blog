2c0ccbaf84f13108fb4c05839172b3e97540121e
gerson C <gersoncalienes@gmail.com>
2017-09-05 14:03:10 -0400

    added models.js

---
+++ models.js
@@ -0,0 +1,34 @@
+const mongoose = require ('require');
+
+
+const blogPostSchema =mongoose.Scheme({
+	suother: {
+		firstName: String,
+		lastName: String
+	},
+	title: {type: String, required: true},
+	content: {type: String},
+	created: {type: Date, default: Date.now}
+});
+
+blogPostSchema.virtual('authorName').get(function() {
+	return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+
+blogPostSchema.methods.apiRepr = function(){
+	return {
+		id:this._id,
+		author: this.authorName,
+		content: this.content,
+		title: this. title,
+		created: this.created
+	};
+}
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = {BlogPost};
+
+
+
